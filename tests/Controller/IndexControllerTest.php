81d7a982c0bbd1123cf5ecbc25d4fe34ff5bf8b9
VladMeh <vladmeh@gmail.com>
2018-04-20 12:30:29 +0300

    add phpunit

---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class IndexControllerTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
