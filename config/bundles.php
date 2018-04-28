d3f7a04b4711d6fbf4688340b98c946abf47ead1
VladMeh <vladmeh@gmail.com>
2018-04-28 17:07:14 +0300

    tcpdf bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    jonasarts\Bundle\TCPDFBundle\TCPDFBundle::class => ['all' => true],
+    WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle::class => ['all' => true],
+    Vladmeh\Bundle\TCPDFBundle\TCPDFBundle::class => ['all' => true],
 ];
