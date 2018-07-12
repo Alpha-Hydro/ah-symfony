459178a8cdb9e26869c514f121cc7dc5dbd632bd
VladMeh <vladmeh@gmail.com>
2018-07-12 15:36:07 +0300

    add role_hierarchy

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,10 @@ class PdfController extends Controller
 {
     /**
      * @Route("/pdf", name="pdf")
+     * @Security("is_granted('ROLE_USER')")
      * @return Response
      */
-    public function index(): Response
+    public function view(): Response
     {
         $html = $this->renderView('pdf/index.html.twig', [
             'controller_name' => 'PdfController',
@@ -28,4 +30,26 @@ class PdfController extends Controller
             'Content-Disposition' => 'inline; filename="new.pdf"'
         ]);
     }
+
+    /**
+     * @Route("/pdf/create", name="pdf-create")
+     * @Security("has_role('ROLE_MANAGER')")
+     * @return Response
+     */
+    public function create(): Response
+    {
+        return new Response('<html><body>Create PDF page! </body></html>');
+    }
+
+    /**
+     * @Route("/pdf/delete", name="pdf-delete")
+     * @Security("has_role('ROLE_ADMIN')")
+     * @return Response
+     */
+    public function delete(): Response
+    {
+        return new Response('<html><body>Delete PDF page! </body></html>');
+    }
+
+
 }
---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by Alpha-Hydro.
+ * @link http://www.alpha-hydro.com
+ * @author Vladimir Mikhaylov <admin@alpha-hydro.com>
+ * @copyright Copyright (c) 2018, Alpha-Hydro
+ *
+ */
+
+namespace App\Security;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $content = "Доступ запрещен";
+
+        return new Response($content, 403);
+    }
+}
