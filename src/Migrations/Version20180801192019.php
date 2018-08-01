893a8b2704bde7cfefc7415def7faee5b21acaf6
vladmeh <vladmeh@gmail.com>
2018-08-02 01:22:53 +0300

    media drop section_site_id thumb autor

---
+++ src/Migrations/Version20180801192019.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180801192019 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE media DROP section_site_id, DROP thumb, DROP autor, CHANGE s_content s_content LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE media ADD section_site_id INT DEFAULT NULL, ADD thumb VARCHAR(255) DEFAULT NULL COLLATE utf8_general_ci, ADD autor VARCHAR(255) DEFAULT NULL COLLATE utf8_general_ci, CHANGE s_content s_content LONGTEXT NOT NULL COLLATE utf8_general_ci');
+    }
+}
